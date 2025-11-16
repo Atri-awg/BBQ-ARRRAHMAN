ed52f81d2df0031233d0b33183657075aac55cfc
Atri-awg <atriwiraguna81@gmail.com>
2025-11-16 22:01:51 +0700

    Add Role model and migration script to database schema

---
+++ api/prisma/migrations/20251116145951_add_role_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "permissions" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
