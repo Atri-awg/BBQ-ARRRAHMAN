9dd95fdcee9823b1638d49549ea50cd5f0adfa8a
AzrRja <alfiazariaroja771@gmail.com>
2025-12-20 23:20:06 +0700

    penambahan text Bimbingan Belajar Al - Qur'an

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -10,6 +10,12 @@ export default function LoginPage() {
     <header className="min-h-screen flex items-center justify-center bg-slate-100">
     <Card className="w-full max-w-md shadow-lg">
       <div className='px-6 pt-8 text-center'>
+        <h2 className='text-2xl font-semibold text-slate-800'>
+          Masuk
+        </h2>
+        <p className='Text-sm text-muted-foreground'>
+          Bimbingan Belajar Al - Qur'an&apos;an
+        </p>
 
       </div>
       
