910b5d82a97600da5cd7a4d7d1acc54acee70cf1
AzrRja <alfiazariaroja771@gmail.com>
2025-12-20 23:15:48 +0700

    Refactor LoginPage layout to enhance UI with Card component and improved styling

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+import { Card } from '@/components/ui/card';
 
 export default function LoginPage() {
 
   return (
     <>
     {/* Header Halaman  */}
-    <header className="px-6 pt-8">
-      <h2 className="text-2xl font-semibold"> Masuk</h2>
-      <p className="text-sm text-muted-foreground">
-        Bimbingan Belajar Al - Quran
-      </p>
+    <header className="min-h-screen flex items-center justify-center bg-slate-100">
+    <Card className="w-full max-w-md shadow-lg">
+      <div className='px-6 pt-8 text-center'>
+
+      </div>
+      
+       </Card>
+
+      
 
     </header>
     
