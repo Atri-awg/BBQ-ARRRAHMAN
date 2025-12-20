5dabb54266330dd9164269af84e518555e2175f3
AzrRja <alfiazariaroja771@gmail.com>
2025-12-20 23:49:28 +0700

    pembuatan form password dan npm

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -2,12 +2,16 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { Mail, Lock } from 'lucide-react';
+import { Mail, Lock, LogIn } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Card } from '@/components/ui/card';
+
+export const metadata = {
+  title: 'Masuk - Bimbingan Belajar Al - Qur\'an',
+};
 
 export default function LoginPage() {
   return (
@@ -25,7 +29,36 @@ export default function LoginPage() {
             Bimbingan Belajar Al - Qur&apos;an
           </p>
         </section>
+    {/* Content */}
+    <section className='px-6 mt-6 space-y-6'>
+      {/* alert */}
+      <div className='flex items-center gap-2 rounded-lg
+                         bg-emerald-50 border border-emerald-200
+                         px-4 py-3 text-sm text-emerald-700'>
+      <LogIn className='h-4 w-4'>
+        Silahkan Login untuk masuk!
+      </LogIn>
+      
+      <form className='space-y-4'>
+        <section className='space-y-1'>
+          <Label>Npm</Label>
+          <div className='relative'> 
+            <Input className='pl-10'></Input>
+          </div>
+          </section> 
 
+          <section className='space-y-1'>
+            <Label>Password</Label>
+            <div className='relative'>
+              <Lock className='absolute left-3 top-2.5 h-4 w-4'></Lock>
+              <Input type='password' className='pl-10'></Input>
+            </div>
+          </section>
+
+      </form>
+
+      </div>
+    </section>
 
         
 
