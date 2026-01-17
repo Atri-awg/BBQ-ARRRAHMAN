ff9dd6752c2ebd75237461309ed486cfc9c620e6
Atri <65006655+Atri-awg@users.noreply.github.com>
2026-01-17 16:12:09 +0700

    feat(layout): update metadata and enhance layout structure with Toaster component

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,21 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'sonner';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'BBQ Ar-Rahman - UKMI',
-  description: 'Sistem Bimbingan Baca Qur\'an',
-}
+  title: 'BBQ Ar-Rahman - Sistem Akademik Bimbingan Baca Quran',
+  description: 'Sistem manajemen akademik untuk bimbingan baca Al-Quran',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="id">
-      <body className={inter.className}>{children}</body>
+    <html lang="id" suppressHydrationWarning>
+      <body className={inter.className}>
+        {children}
+        <Toaster position="top-right" richColors />
+      </body>
     </html>
-  )
+  );
 }
