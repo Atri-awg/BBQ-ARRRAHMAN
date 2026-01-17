f8218211115c09f4d98168fc0644d011fed57995
AzrRja <alfiazariaroja771@gmail.com>
2026-01-17 21:35:10 +0700

    perubahan sedikit

--- web/src/components/logo.tsx
+++ web/src/components/logo.tsx
@@ -1,13 +1,17 @@
-// src/components/Logo.tsx
 import Image from "next/image";
 
-export function Logo({ size = 80 }) {
+type LogoProps = {
+  size?: number;
+};
+
+export function Logo({ size = 80 }: LogoProps) {
   return (
     <Image
-      src="C:\Project Pweb2\BBQ-ARRRAHMAN\web\public\images\bbq logoArtboard 1.png"
+      src="web\public\images\bbq logoArtboard 1.png"
       alt="Logo BBQ Ar-Rahman"
       width={size}
       height={size}
+      priority
     />
   );
 }
